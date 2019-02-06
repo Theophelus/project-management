4b3ccc41df6b6c707a0df539cd2ad7fa7b4815dd
Theophelus <aneletom10@gmail.com>
2019-02-06 15:54:12 +0200

    updating vue routers making sho they working properly

--- client-side/src/App.vue
+++ client-side/src/App.vue
@@ -1,54 +1,37 @@
 <template>
   <div id="app">
-    <div class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+    <div class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
         <a href="../" class="navbar-brand">DASHBOARD</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <router-link to="/" class="nav-link">Home</router-link> 
+              <router-link to="/" class="nav-link">Home</router-link>
             </li>
             <li class="nav-item">
-               <router-link to="/about" class="nav-link">About</router-link>
+              <router-link to="/about" class="nav-link">About</router-link>
             </li>
           </ul>
         </div>
-            <router-view />
       </div>
     </div>
-  </div>
-    <!-- <div id="nav"> -->
-      <!-- <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> -->
-    <!-- </div> -->
+    <router-view/>
   </div>
 </template>
 
-<style>
-
-body {
-  font-family: 'Lato', sans-serif;
-}
-#app {
-  /* font-family: "Avenir", Helvetica, Arial, sans-serif; */
-  /* -webkit-font-smoothing: antialiased; */
-  /* -moz-osx-font-smoothing: grayscale; */
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  name: "app"
+};
+</script>
---
+++ client-side/src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <div class="container">
+          <h1>WELCOME WORLD</h1>
+      </div>    
+  </div> 
+</template>
+
+<script>
+// @ is an alias to /src
+// import Dashboard from "@/components/Dashboard.vue";
+
+export default {
+  name: "home",
+  components: {
+  }
+
+};
+</script>
+
+<style>
+.home h1 {
+
+  margin-top: 60px;
+}
+  
+</style>
+
+
