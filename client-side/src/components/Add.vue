e699e05a388d069e3c63e50d704ba914c8a337f3
Theophelus <aneletom10@gmail.com>
2019-02-08 16:00:32 +0200

    adding an Add.vue component and its structure

---
+++ client-side/src/components/Add.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="add container">
+
+</div>
+    
+</template>
+
+<script>
+export default {
+    name: 'Add',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style scoped>
+
+</style>
+
+
+
+
